133e6d32309da54b4a4221938b29f09ab241e300
mmalini <47175292+mmalini@users.noreply.github.com>
2019-02-13 12:20:00 +0530

    Update greater.py

--- greater.py
+++ greater.py
@@ -2,9 +2,9 @@ a=input().split()
 num1=a[0]
 num2=a[1]
 num3=a[2]
-if num1>num2 and num1>num3:
+if num1>=num2 and num1>=num3:
     print(num1)
-elif num2<num1 and num2<num3:
+elif num2>=num1 and num2>=num3:
     print(num2)
 else:
     print(num3)
